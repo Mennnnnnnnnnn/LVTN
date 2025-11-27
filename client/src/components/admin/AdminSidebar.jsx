cd57f1227b05cc2fcd7c27e0be4d84eff7de33c6
nhutlinh1506 <email_cua_nhutlinh1506@gmail.com>
2025-11-28 02:50:19 +0700

    Add frontend

--- client/src/components/admin/AdminSidebar.jsx
+++ client/src/components/admin/AdminSidebar.jsx
@@ -9,10 +9,10 @@ const AdminSidebar = () => {
         imageUrl: assets.profile,
     }
     const adminNavlinks = [
-        {name: 'Dashboard' , path: '/admin', icon: LayoutDashboardIcon},
-        {name: 'Add Shows' , path: '/admin/add-shows', icon: PlaySquareIcon},
-        {name: 'List Shows' , path: '/admin/list-shows', icon: ListIcon},
-        {name: 'List Bookings' , path: '/admin/list-bookings', icon: ListCollapseIcon},
+        {name: 'Trang tổng quan' , path: '/admin', icon: LayoutDashboardIcon},
+        {name: 'Thêm chương trình ' , path: '/admin/add-shows', icon: PlaySquareIcon},
+        {name: 'Danh sách chương trình' , path: '/admin/list-shows', icon: ListIcon},
+        {name: 'Danh sách đặt chỗ' , path: '/admin/list-bookings', icon: ListCollapseIcon},
     ]
   return (
     <div className='h-[calc(100vh-64px)] md:flex flex-col items-center pt-8 max-w-13 md:max-w-60 w-full border-r border-gray-300/20 text-sm'>
