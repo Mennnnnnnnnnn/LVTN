8f36c8f1c01e46a3c56f93880e86e4e343f6f2b8
nhutlinh1506 <email_cua_nhutlinh1506@gmail.com>
2025-11-29 19:15:41 +0700

    Add frontend

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -17,16 +17,16 @@ const Footer = () => {
                 </div>
                 <div className="flex-1 flex items-start md:justify-end gap-20 md:gap-40">
                     <div>
-                        <h2 className="font-semibold mb-5">Company</h2>
+                        <h2 className="font-semibold mb-5">Công ty</h2>
                         <ul className="text-sm space-y-2">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">About us</a></li>
-                            <li><a href="#">Contact us</a></li>
-                            <li><a href="#">Privacy policy</a></li>
+                            <li><a href="#">Trang chủ</a></li>
+                            <li><a href="#">Về chúng tôi </a></li>
+                            <li><a href="#">Liên hệ với chúng tôi</a></li>
+                            <li><a href="#">Chính sách bảo mật</a></li>
                         </ul>
                     </div>
                     <div>
-                        <h2 className="font-semibold mb-5">Get in touch</h2>
+                        <h2 className="font-semibold mb-5">Hãy liên lạc</h2>
                         <div className="text-sm space-y-2">
                             <p>+1-234-567-890</p>
                             <p>contact@example.com</p>
