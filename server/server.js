69b57cd63fd0a33d5c423f7367ff5e5389e079af
Mennnnnnnnnnn <ongmen51@gmail.com>
2025-12-15 17:04:23 +0700

    Ignore env file and use environment variables

--- server/server.js
+++ server/server.js
@@ -9,10 +9,14 @@ import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
 import adminRouter from './routes/adminRoutes.js';
 import userRouter from './routes/userRoutes.js';
+import { stripeWebhooks } from './controllers/stripeWebhooks.js';
 
 const app = express();
 const port = process.env.PORT || 8080;
 await connectDB();
+
+//stripe webhook route
+app.use('/api/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
 // Middleware
 app.use(express.json());
